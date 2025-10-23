a3b857471a9816814641428240d2203881da74be
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-10-23 11:56:48 +0100

    Update environment for self-approvals

--- environment.sh
+++ environment.sh
@@ -2,12 +2,12 @@
 
 export HOST='local'
 export ENVIRONMENT='local'
-export SERVICE='api'
 
 export FLASK_APP=application.py
 export FLASK_DEBUG=False
 export WERKZEUG_DEBUG_PIN=off
 
+export DATABASE='emergency_alerts'
 export RDS_HOST='host.docker.internal'
 export RDS_PORT=5432
 export RDS_USER='eas-user'
@@ -15,8 +15,13 @@ export RDS_REGION='eu-west-2'
 
 export MASTER_USERNAME=
 export MASTER_PASSWORD=
+
 export API_HOST_NAME=http://host.docker.internal:6011
 export ADMIN_EXTERNAL_URL=http://host.docker.internal:6012
+export ADMIN_ACTION_ALLOW_SELF_APPROVAL=true
+# If running the functional tests on a host - this is the IP of the host from the container's PoV
+# (at least from the perspective of Docker Desktop on macOS)
+export FUNCTIONAL_TEST_IPS='172.18.0.1'
 
 export SECRET_KEY=
 export DANGEROUS_SALT=
