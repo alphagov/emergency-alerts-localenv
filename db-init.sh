cc13038789418b25a28219673164a7e248a4ba34
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-10-23 11:36:12 +0100

    Reuse dev's Ansible to init the DB

--- db-init.sh
+++ db-init.sh
@@ -1,34 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Installing packages to do db initialisation..."
 apt update
-apt install ansible -y
-apt install postgresql-client -y
+apt install ansible postgresql-client python3-psycopg2 -y
+
+set -e
+
+. /build/environment.sh
 
 psql postgresql://postgres:root@host.docker.internal:5432/postgres <<-EOSQL
-    CREATE DATABASE emergency_alerts;
+    CREATE DATABASE $DATABASE;
 EOSQL
 
 psql postgresql://postgres:root@host.docker.internal:5432/postgres -v ON_ERROR_STOP=1 <<-EOSQL
 DO \$\$
 BEGIN
-    IF NOT EXISTS ( SELECT FROM pg_roles WHERE  rolname = 'eas-user') THEN
+    IF NOT EXISTS ( SELECT FROM pg_roles WHERE rolname = 'eas-user') THEN
         CREATE USER "eas-user";
     END IF;
-    GRANT ALL PRIVILEGES ON DATABASE emergency_alerts TO "eas-user";
+
+    GRANT ALL PRIVILEGES ON DATABASE $DATABASE TO "eas-user";
+
     ALTER USER "eas-user" WITH PASSWORD 'password';
 END;
 \$\$;
 EOSQL
 
 cd /build/repos/emergency-alerts-api
-cat /build/environment.sh
-. /build/environment.sh && flask db upgrade
 
-cd /build/repos/emergency-alerts-tooling/ansible/environments/local/
-cat local-postgres-setup.yml | sed "s/127.0.0.1/host.docker.internal/" | sed "s/localhost/host.docker.internal/" >/build/local-postgres-setup.yml
-cd /build
-ansible-playbook local-postgres-setup.yml
+# PostGIS requires superuser to create the extension
+export MASTER_USERNAME='postgres'
+export MASTER_PASSWORD='root'
+
+flask db upgrade
+echo "Flask done"
+
+cd /build/repos/emergency-alerts-tooling/ansible/environments/development
+ansible-playbook -e "database_host=postgres database_username=postgres database_password=root email_address=eas.admin@digital.cabinet-office.gov.uk phone_number=07700900111" 02-database-setup-after-migrations.yml
 
 if [ $? -eq 0 ]
 then
