8d981b9415c3198e946c3f3d35db70521249632e
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-10-23 11:34:10 +0100

    Bump nvm

--- preinstall-npm.sh
+++ preinstall-npm.sh
@@ -2,11 +2,11 @@
 
 apt update
 apt install npm -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 . /root/.bashrc
 . /root/.nvm/nvm.sh
 cd /build/repos/emergency-alerts-admin
-nvm install
+nvm install --lts
 npm ci --no-audit
 
 exit 0
