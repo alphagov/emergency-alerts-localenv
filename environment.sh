07dc17ac3f6b2a2ba02ad9741d8decc73adf062a
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-10-23 11:56:48 +0100

    Update environment for self-approvals

--- environment.sh
+++ environment.sh
@@ -2,12 +2,12 @@
 
 export HOST='local'
 export ENVIRONMENT='local'
-export SERVICE='api'
 
 export FLASK_APP=application.py
 export FLASK_DEBUG=False
 export WERKZEUG_DEBUG_PIN=off
 
+export DATABASE='emergency_alerts'
 export RDS_HOST='host.docker.internal'
 export RDS_PORT=5432
 export RDS_USER='eas-user'
@@ -16,9 +16,11 @@ export MASTER_USERNAME='eas-user'
 export MASTER_PASSWORD='password'
 export API_HOST_NAME=http://host.docker.internal:6011
 export ADMIN_EXTERNAL_URL=http://host.docker.internal:6012
+export ADMIN_ACTION_ALLOW_SELF_APPROVAL=true
 
 export SECRET_KEY='dev-notify-secret-key'
 export DANGEROUS_SALT='dev-notify-salt'
 export ENCRYPTION_DANGEROUS_SALT='dev-notify-salt'
 export ENCRYPTION_SECRET_KEY='dev-notify-secret-key'
 export ADMIN_CLIENT_SECRET='dev-notify-secret-key'
+export FUNCTIONAL_TEST_IPS='172.18.0.1'
