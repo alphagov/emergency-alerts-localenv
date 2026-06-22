c819c653a686eaaa3bc161939fb821b74826e515
Dave-Johnson-GDS <116638871+Dave-Johnson-GDS@users.noreply.github.com>
2026-06-22 14:48:04 +0100

    Add GOVUK_ALERTS_CLOUDFRONT_ENABLED which should be set to false in localstack environments. (#12)

--- environment.sh
+++ environment.sh
@@ -32,6 +32,8 @@ export GOVUK_ALERTS_BLUE_S3_BUCKET_NAME='local-govuk-alerts-blue'
 export GOVUK_ALERTS_GREEN_S3_BUCKET_NAME='local-govuk-alerts-green'
 export GOVUK_ALERTS_ARCHIVE_S3_BUCKET_NAME='local-govuk-alerts-archive'
 export GOVUK_ALERTS_CURRENT_BUCKET_PARAM='govuk-website-current'
+# Cloudfront endpoint not available in free localstack, so disable
+export GOVUK_ALERTS_CLOUDFRONT_ENABLED=false
 export GOVUK_ALERTS_HOST_URL=http://localhost:6017
 export FASTLY_ENABLED=false
 # If running the functional tests on a host - this is the IP of the host from the container's PoV
